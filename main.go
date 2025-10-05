53e71a14a9e60efe59e0156a76035d77b8836b54
viniciuspadovam <padovam3373@gmail.com>
2025-10-05 12:59:54 -0300

    feat: control flow

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Olá, o que deseja fazer?")
 
+	showMenu()
+
+	switch getUserInput() {
+	case 1:
+		fmt.Println("Monitoramento iniciado.")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa.")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando inválido.")
+		os.Exit(-1)
+	}
+}
+
+func showMenu() {
 	fmt.Println("1 - Inicial Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O comando selecionado foi", comando)
+func getUserInput() int {
+	var command int
+	fmt.Scan(&command)
+	return command
 }
